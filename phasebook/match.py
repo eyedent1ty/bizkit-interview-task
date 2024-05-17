4daa6951eb2d42ffeb94f9de031ed8e851e0c337
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-05-17 18:11:54 +0800

    improved the matching algorithm

--- phasebook/match.py
+++ phasebook/match.py
@@ -19,9 +19,15 @@ def match(match_id):
     return {"message": msg, "elapsedTime": end - start}, 200
 
 
-def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+# A helper function used to get all the unique numbers for the given argument
+def getUniqueNumbers(numbers):
+    uniqueNumbers = set() # ensure that there is no duplicate numbers inside the list
+    for number in numbers:
+        uniqueNumbers.add(number)
+    return uniqueNumbers
 
-    return True
+def is_match(fave_numbers_1, fave_numbers_2):
+    arg1 = getUniqueNumbers(fave_numbers_1)
+    arg2 = getUniqueNumbers(fave_numbers_2)
+
+    return arg2.issubset(arg1)
