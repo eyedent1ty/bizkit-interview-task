f80573c398d44fc5f786014a2fcbc877299d3a55
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-05-17 18:12:14 +0800

    implemented the searching algorithm

--- phasebook/search.py
+++ phasebook/search.py
@@ -10,7 +10,6 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
-
 def search_users(args):
     """Search users database
 
@@ -27,4 +26,42 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    if not args:
+        return USERS
+    
+    filtered_users = filter_users(args, USERS)
+    sorted_users = sort_users(args, filtered_users)
+
+    return sorted_users
+
+# A function used to filter users based on the given criteria
+def filter_users(args, users):
+    def filter_key(user):
+        if (
+            args.get('id') == user.get('id') or
+            args.get('name') in user.get('name') or
+            int(args.get('age')) == user.get('age') or
+            args.get('occupation') in user.get('occupation')
+        ):
+            return True
+        return False
+    
+    return list(filter(filter_key, users))
+
+# A function used to sort users based on the order of precedence provided
+def sort_users(args, users):
+    def sorting_key(user):
+        precedence = ['id', 'name', 'age', 'occupation']
+        sorted_criteria = []
+
+        for key in precedence:
+            if key == 'id':
+                sorted_criteria.append(args.get('id') == user.get('id'))
+            elif key == 'age':
+                sorted_criteria.append(int(args.get('age')) == user.get('age'))
+            elif key in ['name', 'occupation']:
+                sorted_criteria.append(args.get(key) in user.get(key, ''))
+
+        return tuple(sorted_criteria)
+    
+    return sorted(users, key=sorting_key, reverse=True)
